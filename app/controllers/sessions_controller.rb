a917af39861a79db07103ed07c1dca058451503d
Phuoc Le <phuocle1990@gmail.com>
2016-03-17 20:28:47 +0700

    milestone 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,10 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out!"
+    redirect_to root_path
+  end
 end
